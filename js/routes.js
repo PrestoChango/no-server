387dff5cc8dcaf53fef629259bf4e61306b47074
preston <mechampreston@gmail.com>
2017-01-23 18:24:32 -0700

    test

--- js/routes.js
+++ js/routes.js
@@ -6,15 +6,15 @@ angular.module('noServer')
     $stateProvider
       .state('home', {
         url: '/',
-        templateUrl: 'views/homepage.html'
+        templateUrl: './no-server/views/homepage.html'
       })
       .state('movie', {
         url: '/movieSearch',
-        templateUrl: 'views/movieSearch.html'
+        templateUrl: './no-server/views/movieSearch.html'
       })
       .state('watch', {
         url: '/reviews',
-        templateUrl: 'views/reviews.html'
+        templateUrl: './no-server/views/reviews.html'
       });
 
 
